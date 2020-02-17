c83aa4dcd8952a31038fc0e5015cc9ea950cb945
JT <48796307+Zrexamourus@users.noreply.github.com>
2020-02-17 09:03:15 -0600

    Add files via upload

---
+++ TreeTraversal/src/node.java
@@ -0,0 +1,17 @@
+import javax.xml.soap.Node;
+
+public class node {
+
+	public int value;
+	public Node left;
+	public Node right;
+	
+	
+	public void Node(int value) {
+		
+	this.value = value;
+	right = null;
+	left = null;
+	
+	}
+}
---
+++ TreeTraversal/src/treeTraversal.java
@@ -0,0 +1,17 @@
+import javax.xml.soap.Node;
+
+public class treeTraversal {
+
+	public Node root;
+	
+	public void Tree(Node node, int value) {
+		if(value < node.value, int value)
+
+	}
+	
+	public static void main(String[] args) {
+
+		
+	}
+
+}
