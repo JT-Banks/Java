41b46bd43526d5781105e5b888b3cb5f123d3864
JT <48796307+Zrexamourus@users.noreply.github.com>
2020-03-16 07:57:39 -0500

    Update Prog01_ExploringJava.java

--- Prog01_ExploreJava/src/Prog01_ExploringJava.java
+++ Prog01_ExploreJava/src/Prog01_ExploringJava.java
@@ -4,37 +4,31 @@
  * @author Josh Bankston
  * @since 1/28/2019
  **/
-
 public class Prog01_ExploringJava {
 
 	public static void main(String[] args) {
 		
 		int first = 3, second = 10;
-
-	//Step 2
+   //Step 2
 		System.out.print("First name: John");
 		System.out.println("Last name: Tyler");
 		
-		System.out.println();
-		
+		System.out.println();	
    //Step 4
 		System.out.println("First name: John");
 		System.out.println("Last name: Tyler");
 		System.out.println();
 		
-  //Step 6
+   //Step 6
 				System.out.print("first name:");
-				System.out.println("John");
-				
- //Step 11
+				System.out.println("John");				
+   //Step 11
 				
 				System.out.println();
 				System.out.println("first="+first);
 				
-//Step 15
+   //Step 15
 				double average=(first+second)/2.0;
 				System.out.println("Average="+average);
-  
-	
 	}
 }
