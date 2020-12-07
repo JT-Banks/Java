8db05475c50b5849e094585c68b184178547ead4
Zrexamourus <48796307+Zrexamourus@users.noreply.github.com>
2020-12-06 21:23:44 -0600

    *
    Change-Id: Ieb74fd05ca2593b3072dd83addd5a03099a1aaa7
    Signed-off-by: Zrexamourus <48796307+Zrexamourus@users.noreply.github.com>

--- escapeRooms/src/Asylum.java
+++ escapeRooms/src/Asylum.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 import java.util.Timer;
 
 public class Asylum {
+	
 	static Timer time = new Timer();
 	public static String user;
 	actions act = new actions();
@@ -14,6 +15,7 @@ public class Asylum {
 		System.out.println("Welcome " +user+ "! \nWhen you're ready to begin, just type !start");
 		String start = in.findInLine("!start");
 	}
+	
 	public void passages() {
 
 		ArrayList <Integer> doors = new ArrayList<>();
@@ -27,9 +29,10 @@ public class Asylum {
 		int staffLocker2 = 2;
 		int staffLocker3 = 3;
 		int janitorCloset = 4;
-		}	
+	}	
+
 	public static void game() {
-		
+
 		Scanner in = new Scanner(System.in);
 		System.out.println("|*|*|*|*|*|*|*|*|*|*|*|");
 		System.out.println("Welcome to the Asylum!");
@@ -38,15 +41,14 @@ public class Asylum {
 		System.out.println("You have 6 minutes to complete this challenge.");
 		System.out.println();
 		System.out.println("Every room you enter will display"
-							+ "a list of items of interest, to investigate items further simply type a '!' followed by the item name \nSuch as: !toolbox."
-							+ " Once information is displayed, you can use this information to discover further clues, if you get stuck, at any time you can"
-							+ "type '!return' to return to the intial room. From this room ONLY are hints available, once in the room, simply type"
-							+ "'!hint'");
+				+ "a list of items of interest, to investigate items further simply type a '!' followed by the item name \nSuch as: !toolbox."
+				+ " Once information is displayed, you can use this information to discover further clues, if you get stuck, at any time you can"
+				+ "type '!return' to return to the intial room. From this room ONLY are hints available, once in the room, simply type"
+				+ "'!hint'");
 		System.out.println();
 		int input = in.nextInt();
 		switch(input) {
 		case 1: 
 		}
 	}
-
 }
