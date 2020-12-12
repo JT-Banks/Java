e862519aedc49225785a6ddb58f79f57a1f2ead9
Zrexamourus <48796307+Zrexamourus@users.noreply.github.com>
2020-12-12 15:05:47 -0600

    Clean up

--- CarsProject/src/Car.java
+++ CarsProject/src/Car.java
@@ -1,27 +1,32 @@
 public class Car {   
-    //Declaring private variables
-    private long vin;
-    private String model;
-    private int year;
-    private double mileage; 
-    //Default Constructor that is empty and doesn't return anything
-    public Car(){ 
-    }
-    //Constructor for VIN number, model, and year
-    public Car (long vinNum, int yearC, String modelC) {
-        //Setting individual values for specific cars
-        vin = vinNum;
-        model = modelC;
-        year = yearC;  
-    }
-    public void setMileage(double Mileage) {
-       //Method to set mileage, which should be changeable, and not private
-       mileage = Mileage;
-    }
-    //Method to get info
-    public String getInfo() {
-        
-        return year+" "+ model + " "+ "(VIN: " + vin + ", mileage: "+ mileage +" miles)";
-    }
+
+	//Declaring private variables
+	private long vin;
+	private String model;
+	private int year;
+	private double mileage; 
+
+	//Default Constructor that is empty and doesn't return anything
+	public Car(){
+
+	}
+	//Constructor for VIN number, model, and year
+	public Car (long vinNum, int yearC, String modelC) {
+		//Setting individual values for specific cars
+		vin = vinNum;
+		model = modelC;
+		year = yearC;  
+	}
+
+	public void setMileage(double Mileage) {
+		//Method to set mileage, which should be changeable, and not private
+		mileage = Mileage;
+	}
+
+	//Method to get info
+	public String getInfo() {
+
+		return year+" "+ model + " "+ "(VIN: " + vin + ", mileage: "+ mileage +" miles)";
+	}
 }
 
